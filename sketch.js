2030685c0ecfce999459c16e3fac05cff5430298
Bryden Ong <71949858+MEGABO2008@users.noreply.github.com>
2020-10-20 17:07:20 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+var car, wall;
+var speed, weight;
+
+
+
+
+function setup() {
+  createCanvas(1600,400);
+  car=createSprite(50, 200, 50, 50);
+  car.velocityX=speed;
+  wall=createSprite(1500,200,60,height/2);
+  wall.shapeColor(80,80,80);
+
+car,shapeColor =color(255);
+  speed=random(55,90);
+  weight=random(400,1500);
+}
+
+function draw() {
+  background(0);  
+
+
+
+
+
+
+drawSprites();
+}
